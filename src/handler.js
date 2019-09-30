ea873a9d4034e89f5f6ec1eabbf8c9a00180dce2
Rodion Chachura <geekrodion@gmail.com>
2019-09-30 17:58:42 +0300

    update: return arns

--- src/handler.js
+++ src/handler.js
@@ -1,3 +1,7 @@
 module.exports = async (path, headers, body) => {
-  return { path, headers, body }
+  console.log(path, headers, body)
+  return { 
+    TOPIC_ARN_BOUNCE: process.env.TOPIC_ARN_BOUNCE,
+    TOPIC_ARN_COMPLAINT: process.env.TOPIC_ARN_COMPLAINT
+  }
 }
