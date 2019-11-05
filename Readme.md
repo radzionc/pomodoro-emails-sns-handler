f8e6ee68727fb83e8ecd649836a1539cd37c5fc4
Rodion Chachura <geekrodion@gmail.com>
2019-11-05 05:32:16 +0300

    feature: readme

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# pomodoro-emails-sns-handler
+
+>
+
+![all text](https://cdn-images-1.medium.com/max/800/1*-Ep6APdMkZYHMHxBqdUZBw.png)
+
+## [Medium Story](https://medium.com/p/9ad89939a158)
+
+## License
+
+MIT © [RodionChachura](https://geekrodion.com)
--- management/deploy.sh
+++ management/deploy.sh
@@ -3,6 +3,5 @@ BUCKET_KEY=lambda.zip
 chmod -R +x ./
 zip -r ./$BUCKET_KEY *
 
-echo $AWS_ACCESS_KEY_ID 
 aws s3 cp $BUCKET_KEY s3://$BUCKET/$BUCKET_KEY
 aws lambda update-function-code --function-name tf-pomodoro-emails-sns-handler --s3-bucket $BUCKET --s3-key $BUCKET_KEY
